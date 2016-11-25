a7a1ce7a383f9e3cb2bf8545efea19b2a1a04dd1
esteban <esteban@StevePC>
2016-11-25 06:02:23 -0600

    Agregado de los objetos que serán almacenados dentro del grafo del mundo

---
+++ src/Fabricas/Mina.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Fabricas;
+
+/**
+ *
+ * @author esteban
+ */
+public class Mina extends Fabrica{
+
+    public Mina(Orientacion orientacionFabrica, int posicionX, int posicionY) {
+        super(orientacionFabrica, posicionX, posicionY);
+    }
+}
---
+++ src/Fabricas/Templo.java
@@ -0,0 +1,18 @@
+package Fabricas;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author esteban
+ */
+public class Templo extends Fabrica{
+
+    public Templo(Orientacion orientacionFabrica, int posicionX, int posicionY) {
+        super(orientacionFabrica, posicionX, posicionY);
+    }
+}
